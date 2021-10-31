d00e689761007a9d8d67d282efa925e78fe21727
luanbrito <luan@viitra.com>
2021-10-31 13:50:46 -0300

    add CNN model w precision and recall functions

--- MLP_Handwritten_Digits/test.py
+++ MLP_Handwritten_Digits/test.py
@@ -9,7 +9,61 @@ training_data = list(training_data)
 # - mlp.py example:
 import mlp, time
 
-currentTime = time.time()
-net = mlp.MLP([784, 50, 50, 10])
-net.SGD(training_data, 30, 10, 3.0, test_data=test_data)
-print("--- %s seconds ---" % (time.time() - currentTime))
+# currentTime = time.time()
+# net = mlp.MLP([784, 30, 10])
+# net.SGD(training_data, 30, 10, 4.0, test_data=test_data)
+# print("--- %s seconds ---" % (time.time() - currentTime))
+
+import network3
+from network3 import Network, ConvPoolLayer, FullyConnectedLayer, SoftmaxLayer # softmax plus log-likelihood cost is more common in modern image classification networks.
+
+# read data:
+training_data, validation_data, test_data = network3.load_data_shared()
+# mini-batch size:
+mini_batch_size = 10
+
+# chapter 6 - shallow architecture using just a single hidden layer, containing 100 hidden neurons.
+net = Network([
+    FullyConnectedLayer(n_in=784, n_out=100),
+    SoftmaxLayer(n_in=100, n_out=10)], mini_batch_size)
+net.SGD(training_data, 60, mini_batch_size, 0.1, validation_data, test_data)
+
+# chapter 6 - 5x5 local receptive fields, 20 feature maps, max-pooling layer 2x2
+'''
+net = Network([
+    ConvPoolLayer(image_shape=(mini_batch_size, 1, 28, 28),
+                  filter_shape=(20, 1, 5, 5),
+                  poolsize=(2, 2)),
+    FullyConnectedLayer(n_in=20*12*12, n_out=100),
+    SoftmaxLayer(n_in=100, n_out=10)], mini_batch_size)
+net.SGD(training_data, 60, mini_batch_size, 0.1, validation_data, test_data)
+'''
+
+# chapter 6 - inserting a second convolutional-pooling layer to the previous example => better accuracy
+'''
+net = Network([
+    ConvPoolLayer(image_shape=(mini_batch_size, 1, 28, 28),
+                  filter_shape=(20, 1, 5, 5),
+                  poolsize=(2, 2)),
+    ConvPoolLayer(image_shape=(mini_batch_size, 20, 12, 12),
+                  filter_shape=(40, 20, 5, 5),
+                  poolsize=(2, 2)),
+    FullyConnectedLayer(n_in=40*4*4, n_out=100),
+    SoftmaxLayer(n_in=100, n_out=10)], mini_batch_size)
+net.SGD(training_data, 60, mini_batch_size, 0.1, validation_data, test_data)
+'''
+
+# chapter 6 -  rectified linear units and some l2 regularization (lmbda=0.1) => even better accuracy
+# from network3 import ReLU
+# net = Network([
+#     ConvPoolLayer(image_shape=(mini_batch_size, 1, 28, 28),
+#                   filter_shape=(20, 1, 5, 5),
+#                   poolsize=(2, 2),
+#                   activation_fn=ReLU),
+#     ConvPoolLayer(image_shape=(mini_batch_size, 20, 12, 12),
+#                   filter_shape=(40, 20, 5, 5),
+#                   poolsize=(2, 2),
+#                   activation_fn=ReLU),
+#     FullyConnectedLayer(n_in=40*4*4, n_out=100, activation_fn=ReLU),
+#     SoftmaxLayer(n_in=100, n_out=10)], mini_batch_size)
+# net.SGD(training_data, 60, mini_batch_size, 0.03, validation_data, test_data, lmbda=0.1)
